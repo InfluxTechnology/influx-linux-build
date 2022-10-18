34036ac04e7b7c66a706f11fb517b7c8ba751a81
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2022-10-18 17:14:24 +0300

    Delete sources/meta-ea/recipes-ea/ea-files/ea-files/other directory

--- sources/meta-ea/recipes-ea/ea-files/ea-files/other/cust_log.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-DATE=$(/bin/date)
-echo $DATE "|" $1 >> /opt/influx/etc/custom.log
--- sources/meta-ea/recipes-ea/ea-files/ea-files/other/custom_reboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# after reboot, may  see real booting time with: cat /opt/influx/etc/custom.log
-
-echo "" > /opt/influx/etc/custom_reboot 
-/opt/influx/cust_log.sh "reboot" && reboot
-
-
